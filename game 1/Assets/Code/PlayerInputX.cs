78155317dba7703c8ad5052234eccd85d656d304
Alxcgs <gerasymchuk.os@gmail.com>
2024-11-24 16:02:32 +0200

    Asets

---
+++ game 1/Assets/Code/PlayerInputX.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Code
+{
+    public class PlayerInputX : MonoBehaviour
+    {
+        private const string AxisHorizontal = "Horizontal";
+
+        [SerializeField]
+        private MoverX _mover;
+        private void Update()
+        {
+            float input = Input.GetAxis(AxisHorizontal);
+            _mover.Move(input);
+        }
+    }
+}
